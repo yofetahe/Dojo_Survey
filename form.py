0470192149a96acd6ebdc912e94086ade78febd1
Yofatehe <42419407+yofetahe@users.noreply.github.com>
2019-02-11 16:05:19 -0800

    database connection

--- form.py
+++ form.py
@@ -1,11 +1,35 @@
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, SelectField, TextAreaField, RadioField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
+from mysqlconnection import connectToMySQL
+
+def getLocationList():
+    my_sql = connectToMySQL('flask_pets')
+    return my_sql.query_db("select * from locations")
+
+def getLanguageList():
+    my_sql = connectToMySQL('flask_pets')
+    return my_sql.query_db("select * from languages")
 
 class SurveyForm(Form):
     name = StringField('Your Name: ', validators=[DataRequired("Please enter your name")])
     gender = RadioField('Gender', choices=[('M', 'Male'), ('F', 'Female'), ('NN', 'Dont Need to answer')], validators=[DataRequired("Gender is required")])
-    location = SelectField('Dojo Location: ', choices=[('',''), ('SJ', 'San Jose'), ('WA', 'Washington')], validators=[DataRequired("Please select the location")])
-    language = SelectField('Favourite Language: ', choices=[('', ''), ('Py','Python'), ('JS','JavaScript')], validators=[DataRequired("Please select language")])
-    comment = TextAreaField('Comment (optional): ')
-    submit = SubmitField('Button')
+    
+    # >>>> to create dynamic choice list for SelectedField <<<< #
+    locList = getLocationList()
+    locOptions = [('', '')]    
+    for loc in locList:        
+        locOptions.append((loc['id'] , loc['name']))
+    # >>>> to create dynamic choice list for SelectedField <<<< #    
+    location = SelectField('Dojo Location: ', choices=locOptions, validators=[DataRequired("Please select the location")])
+    
+    # >>>> to create dynamic choice list for SelectedField <<<< #
+    lanList = getLanguageList()
+    lanOptions = [('', '')]    
+    for lan in lanList:        
+        lanOptions.append((lan['id'] , lan['name']))
+    # >>>> to create dynamic choice list for SelectedField <<<< #
+    language = SelectField('Favourite Language: ', choices=lanOptions, validators=[DataRequired("Please select language")])
+    
+    comment = TextAreaField('Comment (Optional): ', validators=[Length(max=120, message="The comment should be less than 120 characters")])
+    submit = SubmitField('Submit')
--- server.py
+++ server.py
@@ -1,28 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from form import SurveyForm
+from mysqlconnection import connectToMySQL
 
 app = Flask('__name__')
 
 app.secret_key = "development_key"
 
 @app.route("/index", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-
     form = SurveyForm()
-    
-    if request.method == 'GET':
+    return render_template("index.html", form=form)
+        
+@app.route("/save_survey", methods=['POST'])
+def save_survey():
+    form = SurveyForm()
+    if form.validate == False:
         return render_template("index.html", form=form)
-    elif request.method == 'POST':
-        if form.validate == False:
-            return render_template("index.html", form=form)
-        else:
-            name = form.name.data
-            gender = form.gender.data
-            location = form.location.data
-            language = form.language.data
-            comment = form.comment.data
-            return render_template("detail.html", name=name, gender=gender, location=location, language=language, comment=comment)
-   
+    else:
+        my_sql = connectToMySQL('flask_pets')
+        query = "INSERT INTO dojo_survey(name, gender, location, language, comment, created_at) VALUES(%(name)s, %(gender)s, %(location)s, %(language)s, %(comment)s, NOW())"
+        data = {
+            "name": form.name.data,
+            "gender": form.gender.data,
+            "location": form.location.data,
+            "language":  form.language.data,
+            "comment": form.comment.data
+        }
+        id = my_sql.query_db(query, data)
+        return redirect("/survey_detail/"+str(id))
+
+@app.route("/survey_detail/<id>")
+def survey_detail(id):
+    my_sql = connectToMySQL('flask_pets')
+    query = "SELECT sur.id, sur.name, sur.gender, lan.name as language, loc.name as location, sur.comment FROM languages lan join dojo_survey sur on sur.language = lan.id join locations loc on sur.location = loc.id WHERE sur.id = %(id)s"
+    data = {
+        "id": id
+    }
+    surveyInfo = my_sql.query_db(query, data)
+    return render_template("/detail.html", surveyInfo=surveyInfo)
 
 if __name__ == '__main__':
     app.run(debug=True)
